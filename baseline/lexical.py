8bb111ee1c35c8a3c86b3dc5669f5168b3579624
Hamza Keurti <hamza.keurti@gmail.com>
2019-05-28 15:55:57 +0800

    Added logging handler

--- baseline/lexical.py
+++ baseline/lexical.py
@@ -20,21 +20,21 @@ def parse_lines(lines):
 
 
 
-def parse_dadb(filename):
-    with open(filename,'r') as file:
-        line = file.readline()
-        k=0
-        while line:
-            line_split = line.split(',')
-            begin_time,end_time = float(line_split[0]),float(line_split[1])
-            label = line_split[-9]
-            words = [x.split('+')[-1] for x in line_split[4].split('|')]
+# def parse_dadb(filename):
+#     with open(filename,'r') as file:
+#         line = file.readline()
+#         k=0
+#         while line:
+#             line_split = line.split(',')
+#             begin_time,end_time = float(line_split[0]),float(line_split[1])
+#             label = line_split[-9]
+#             words = [x.split('+')[-1] for x in line_split[4].split('|')]
 
 
-            data.append(words)
-            labels.append(label)
-            frames.append(librosa.core.time_to_frames((begin_time,end_time)))
+#             data.append(words)
+#             labels.append(label)
+#             frames.append(librosa.core.time_to_frames((begin_time,end_time)))
 
-            line = file.readline()
-    return data,labels,frames
+#             line = file.readline()
+#     return data,labels,frames
 
