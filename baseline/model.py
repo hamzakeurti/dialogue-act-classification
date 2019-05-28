5d9f9fc4e7273d3114f3d06901e8da3aa7a3b4a2
Hamza Keurti <hamza.keurti@gmail.com>
2019-05-28 15:00:54 +0800

    Corrected lexical model for shapes

--- baseline/model.py
+++ baseline/model.py
@@ -18,30 +18,26 @@ class LexicalModel(nn.Module):
         self.lstm = nn.LSTM(
             input_size = self.conv_channels,
             hidden_size = self.lstm_hidden_dim,
-            batch_first = True) 
+            batch_first = False) 
 
         # self.fc = nn.Linear(hidden_dim, output_dim)
         self.device = device
 
     def forward(self, text):
-        # TODO: your codes here
-        # text: [num sent, batch size, sent len]
-        
         # 1. get embdding vectors
-        # embedded: [num sent, batch size, sent len, emb dim]
-        embedded = self.embedding(text)
-
+        # embedded: [num sent, batch size, emb dim, sent len]
+        embedded = self.embedding(text).permute(0,1,3,2)        
+        
         # 2. convolution over each sentence
         conv_outputs = [] 
-
         for i in range(self.context_size):
             conv_outputs.append(self.conv(embedded[i]))
-        # conv_output: [num sent, batch size, sent len, num_channels]
+        # conv_output: [num sent, batch size, num channels, sent len]
         conv_output = torch.stack(conv_outputs)
-
+        
         # 3. MaxPool the whole sentence into a single vector of dim num channels
         # max_output: [num sent, batch size, num_channels]
-        max_output = torch.max(conv_output,dim = 3)  #max over sentence length
+        max_output,_ = torch.max(conv_output,dim = 3)  #max over sentence length
 
         # 4. LSTM the 3 sentences to determine attention 
         # lstm_output: [num sent, batch size, num_channels]
