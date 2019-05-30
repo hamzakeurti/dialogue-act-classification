f79939bd1283181a1fe860e1e3011482df0a1119
Hamza Keurti <hamza.keurti@gmail.com>
2019-05-30 17:49:16 +0800

    Attention mechanism incorporated in lexical model

--- baseline/model.py
+++ baseline/model.py
@@ -1,6 +1,54 @@
 import torch.nn as nn
 import torch
 
+# class LexicalModel(nn.Module):
+#     def __init__(self, vocab_size, embedding_dim = 300, conv_channels=256, output_dim=128, kernel_size=5, context_size=3, device='cpu',init_embedding = torch.randn((14600,300))):
+#         super(LexicalModel, self).__init__()
+#         self.vocab_size = vocab_size          # sentence_length
+#         self.context_size = context_size    # Number of sentences provided as context including current sentence.
+#         self.embedding_dim = embedding_dim
+#         self.conv_channels = conv_channels
+#         self.output_dim = output_dim 
+#         self.kernel_size = kernel_size
+
+#         self.embedding = nn.Embedding(vocab_size, embedding_dim)
+
+#         self.conv = nn.Conv1d(self.embedding_dim,self.conv_channels,self .kernel_size)
+
+#         self.lstm = nn.LSTM(
+#             input_size = self.conv_channels,
+#             hidden_size = self.output_dim,
+#             batch_first = False) 
+
+#         # self.fc = nn.Linear(hidden_dim, output_dim)
+#         self.device = device
+        
+#         self.embedding.weight.data.copy_(init_embedding)
+#     def forward(self, text):
+#         # 1. get embdding vectors
+#         # embedded: [num sent, batch size, emb dim, sent len]
+#         embedded = self.embedding(text.long()).permute(1,0,3,2)        
+
+#         # 2. convolution over each sentence
+#         conv_outputs = [] 
+#         for i in range(self.context_size):
+#             conv_outputs.append(self.conv(embedded[i]))
+#         # conv_output: [num sent, batch size, num channels, sent len]
+#         conv_output = torch.stack(conv_outputs)
+
+#         # 3. MaxPool the whole sentence into a single vector of dim num channels
+#         # max_output: [num sent, batch size, num_channels]
+#         max_output,_ = torch.max(conv_output,dim = 3)  #max over sentence length
+
+#         # 4. LSTM the 3 sentences to determine attention 
+#         # lstm_output: [num sent, batch size, output dim]
+#         lstm_output, _ = self.lstm(max_output)
+        
+#         # 5. Sum the resulting vectors
+#         ret = torch.sum(lstm_output,dim=0)
+#         return ret
+
+
 class LexicalModel(nn.Module):
     def __init__(self, vocab_size, embedding_dim = 300, conv_channels=256, output_dim=128, kernel_size=5, context_size=3, device='cpu',init_embedding = torch.randn((14600,300))):
         super(LexicalModel, self).__init__()
@@ -20,10 +68,12 @@ class LexicalModel(nn.Module):
             hidden_size = self.output_dim,
             batch_first = False) 
 
-        # self.fc = nn.Linear(hidden_dim, output_dim)
+        self.attention_fc = nn.Linear(self.output_dim, self.output_dim)
         self.device = device
         
         self.embedding.weight.data.copy_(init_embedding)
+
+        
     def forward(self, text):
         # 1. get embdding vectors
         # embedded: [num sent, batch size, emb dim, sent len]
@@ -44,9 +94,20 @@ class LexicalModel(nn.Module):
         # lstm_output: [num sent, batch size, output dim]
         lstm_output, _ = self.lstm(max_output)
         
-        # 5. Sum the resulting vectors
-        ret = torch.sum(lstm_output,dim=0)
+        # 5. Activation units
+        activations = [] 
+        for i in range(self.context_size):
+            activations.append(self.attention_fc(lstm_output[i]))
+        # activations: [num sent, batch size, num channels, sent len]
+        activation = torch.stack(activations)
+
+        # 6. Compute attention
+        attention = nn.Softmax(activation,dim=0)
+
+        # 7. Sum the resulting vectors weighted by attention
+        ret = torch.sum((attention * lstm_output),dim=0)
         return ret
+
 
  
 class AcousticModel(nn.Module):
